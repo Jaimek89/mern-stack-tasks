9b81568167480257c7b54917db4953f32aa08f02
Jaimek89 <jaimek89@gmail.com>
2019-06-04 13:31:40 +0100

    added routes & static files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // Express file - Node.js
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 // Settings
+app.set('port', process.env.PORT || 3000)
 
 // Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
 
 // Routes
+app.use('/api/tasks', require('./routes/task.routes'))
 
 // Static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Starting server
-app.listen(3000, () => {
-    console.log('server on port 3000')
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`)
 })
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,1 +1,11 @@
 // Define the URL for the server - CRUD
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({
+        status: 'API Works'
+    })
+})
+
+module.exports = router
