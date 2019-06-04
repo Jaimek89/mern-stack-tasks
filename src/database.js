07314b8728bea2bc72254f292167fe35163ba390
Jaimek89 <jaimek89@gmail.com>
2019-06-04 15:30:31 +0100

    added CRUD routes& finish backend

--- src/database.js
+++ src/database.js
@@ -1,1 +1,12 @@
 // Our way to connect with the database
+const mongoose = require('mongoose')
+
+const URI = 'mongodb://localhost/mern-tasks'
+
+mongoose.connect(URI, {
+    useNewUrlParser: true
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err))
+
+module.exports = mongoose
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,10 +2,40 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API Works'
-    })
+const Task = require('../models/task')
+
+// Get method (to collect all the tasks)
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
 })
 
-module.exports = router
+// Get one method (to collect just one task through it's ID)
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id)
+    res.json(task)
+})
+
+// Post method (To post a new task)
+router.post('/', async (req, res) => {
+    const { title, description } = req.body
+    const task = new Task({ title, description })
+    await task.save()
+    res.json({ status: 'Task Saved' })
+})
+
+// Put method (To update a task)
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body
+    const newTask = { title, description }
+    await Task.findByIdAndUpdate(req.params.id, newTask)
+    res.json({ status: 'Task Updated'})
+})
+
+// Delete method (To delete a task)
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id)
+    res.json({ status: 'Task Deleted' })
+})
+
+module.exports = router 
