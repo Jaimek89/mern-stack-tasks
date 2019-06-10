c8ac7cf66f906217fe753d3d1ce542179d2693b8
Jaimek89 <jaimek89@gmail.com>
2019-06-10 11:51:47 +0100

    changes for heroku 2

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 // Our way to connect with the database
 const mongoose = require('mongoose')
 
-const URI = MONGODB_URI //'mongodb://localhost/mern-tasks' // To upload our App, URI needs to be undefined, because URI will be get from MongoDB Atlas
+const URI = undefined
+//'mongodb://localhost/mern-tasks' // To upload our App, URI needs to be undefined, because URI will be get from MongoDB Atlas
 
 mongoose.connect(URI, {
     useNewUrlParser: true
