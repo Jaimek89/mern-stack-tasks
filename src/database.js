05201a084d5e14bf5529ce2d39ebcbc7ec864d14
Jaimek89 <jaimek89@gmail.com>
2019-06-12 18:50:24 +0100

    removed mlab server

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
 // Our way to connect with the database
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://jaimek89:jaimek89123@ds037145.mlab.com:37145/mern-stack-tasks-jaimek89'
-//'mongodb://localhost/mern-tasks'
+const URI = 'mongodb://localhost/mern-tasks'
 
 mongoose.connect(URI, {
     useNewUrlParser: true
